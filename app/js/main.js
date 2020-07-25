9bb04b0d5468c0557f78a0431be05b7d0bc01fb1
Tiutiunnyk-Liliia <yalili4ka1210@gmail.com>
2020-07-25 17:56:16 +0300

    Developing section "slider-block" is done

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,8 @@
 $(function () {
-
+    $('.slider').slick({
+        dots: true,
+        prevArrow: '<img class="slider-arrows slider-arrows-left" src="../img/prev-arrov.png" alt="">',
+        nextArrow: '<img class="slider-arrows slider-arrows-right" src="../img/next-arrow.png" alt="">',
+        infinite: false,
+    })
 });
