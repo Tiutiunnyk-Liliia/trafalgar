ac438117048a9556ac936fe6b1ea2c5f58cf86f2
Tiutiunnyk-Liliia <yalili4ka1210@gmail.com>
2020-07-30 20:01:19 +0300

    Developing header media version is done

--- app/js/main.js
+++ app/js/main.js
@@ -4,5 +4,11 @@ $(function () {
         prevArrow: '<img class="slider-arrows slider-arrows-left" src="../img/prev-arrov.png" alt="">',
         nextArrow: '<img class="slider-arrows slider-arrows-right" src="../img/next-arrow.png" alt="">',
         infinite: false,
-    })
+    });
+
+    $('.header__menu-btn').on('click', function () {
+        $('.header__menu').toggleClass('active')
+        $('.header__menu-btn').toggleClass('active')
+
+    });
 });
